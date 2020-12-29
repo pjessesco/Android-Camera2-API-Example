817256b84b933683deb536f6b1919bc124d447db
Jino Park <pjessesco@gmail.com>
2020-12-29 13:12:03 +0900

    Keep device awake

--- app/src/main/java/com/jack/mainactivity/MainActivity.java
+++ app/src/main/java/com/jack/mainactivity/MainActivity.java
@@ -29,6 +29,7 @@ import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -86,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         textureView = (TextureView) findViewById(R.id.texture);
         assert textureView != null;
         textureView.setSurfaceTextureListener(textureListener);
